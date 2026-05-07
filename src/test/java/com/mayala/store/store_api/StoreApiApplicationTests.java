60bb05a566ad83072f038cc5ab142e48d7167dd3
mayala <mayala@digitalgeko.com>
2026-05-06 21:11:43 -0600

    initial commit

---
+++ src/test/java/com/mayala/store/store_api/StoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayala.store.store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
